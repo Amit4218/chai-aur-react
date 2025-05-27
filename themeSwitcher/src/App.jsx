9558ca80ed392e4e119499654095b763f0a11a85
Amit4218 <amitbhagat621@gmail.com>
2025-05-27 19:36:58 +0530

    ThemeSwitcher for practicing context api concept

---
+++ themeSwitcher/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { ThemeContextProvider } from "./contexts/ThemeContext";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightMode = () => {
+    setThemeMode("light");
+  };
+
+  const darkMode = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("body").classList.remove("light", "dark");
+    document.querySelector("body").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeContextProvider value={{ themeMode, darkMode, lightMode }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeContextProvider>
+  );
+}
+
+export default App;
